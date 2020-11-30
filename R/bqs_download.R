a1490dcaa492ca0f394c54888f2946df36da71a9
Bruno Tremblay <meztez@neoxone.com>
2020-11-30 02:45:39 -0500

    better root cert detection

--- R/bqs_download.R
+++ R/bqs_download.R
@@ -139,8 +139,11 @@ overload_bq_table_download <- function(parent) {
 
 grpc_mingw_root_pem_path_detect <-
   if (Sys.info()[["sysname"]] == "Windows") {
-    file.path(Sys.getenv("RTOOLS40_HOME"),
-              if (Sys.info()[["machine"]] == "x86-64") {"mingw64"} else {"mingw32"},
+    RTOOLS40_ROOT <- gsub("\\\\", "/", Sys.getenv("RTOOLS40_HOME", "c:/rtools40"))
+    WIN <- if (Sys.info()[["machine"]] == "x86-64") {"64"} else {"32"}
+    MINGW_PREFIX <- paste0("mingw", WIN)
+    file.path(RTOOLS40_ROOT,
+              MINGW_PREFIX,
               "share",
               "grpc",
               "roots.pem")
