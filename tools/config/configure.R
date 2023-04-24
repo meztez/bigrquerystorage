8dc5e930d951c3da67f2e90cb50ff7c40edd9d09
Bruno Tremblay <bruno.tremblay@lacapitale.com>
2023-04-24 08:57:02 -0400

    add protoc

--- tools/config/configure.R
+++ tools/config/configure.R
@@ -34,6 +34,7 @@ if (win) {
 		Sys.setenv(PATH = paste(BINPREF, Sys.getenv("PATH"), sep = ";"))
 		if (Sys.which("grpc_cpp_plugin") == "") {
 			# attempt to install grpc and protoc using msys2
+			install_with_pacman("protoc", RTOOLS43_ROOT, WIN)
 			install_with_pacman("grpc", RTOOLS43_ROOT, WIN)
 		}
 		message(" OK")
