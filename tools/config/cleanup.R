eff0841f0cfecc1537690375ceb19108cceabcf8
Jeroen Ooms <jeroenooms@gmail.com>
2023-12-09 14:18:56 +0100

    Overhaul build setup

--- tools/config/cleanup.R
+++
@@ -1,6 +0,0 @@
-# Clean up files generated during configuration here.
-# Use 'remove_file()' to remove files generated during configuration.
-
-unlink(dir("./src", c(".o$", ".dll$"), recursive = TRUE, full.names = TRUE))
-unlink("./src/google", recursive = TRUE)
-unlink("./src/Makevars")
